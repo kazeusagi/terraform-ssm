24ca7544db68406279999a50c6ceff3eb06e1190
kazeusagi <toshiki1098@gmail.com>
2025-09-10 00:24:27 +0000

    feat: cicd

--- terraform/environments/usecase-3/main.tf
+++ terraform/environments/usecase-3/main.tf
@@ -1,8 +1,11 @@
 # OIDC: CI/CDからAssumeRoleする際の権限等の設定
 module "iam_oidc_provider" {
-  source                      = "../../modules/oidc/provider"
-  allowed_github_repositories = ["repo:HC-Produce/edinet-scraper:environment:dev"]
-  allow_ssm                   = true
+  source = "../../modules/oidc/provider"
+  allowed_github_repositories = [
+    "repo:kazeusagi/terraform-ssm:environment:dev",
+    "repo:kazeusagi/terraform-ssm:environment:prod"
+  ]
+  allow_ssm = true
 }
 
 
